ffe7b69af9ba1f0be4cae6cd9cdb6fbba4a7aae8
sunny <dance@kakao.com>
2023-12-09 22:44:38 +0900

    🔧setting next/Image on Card, setting next.config (images of domain)

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['images.unsplash.com', 'github.com', 'avatars.githubusercontent.com'],
+  }
+}
 
 module.exports = nextConfig
