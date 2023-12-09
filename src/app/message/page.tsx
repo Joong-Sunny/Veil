98ab0fcc36c8da3bb175b96c4a87c5bb337d62bd
sunny <dance@kakao.com>
2023-12-09 22:42:29 +0900

    🚀setting PhoneFrame on layout

---
+++ src/app/message/page.tsx
@@ -0,0 +1,11 @@
+import Spacing from "@/app/components/Spacing";
+
+export default function Home() {
+  return (
+    <>
+      <span>보낸메시지가 아직 없어요!!</span>
+      <Spacing direction="vertical" size={300} />
+      <span>받은메시지가 아직 없어요!!</span>
+    </>
+  )
+}
